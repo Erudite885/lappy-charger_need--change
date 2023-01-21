302d00da2e75711d0843e1c5cd54a765502f2137
Erudite885 <erudite885@gmail.com>
2023-01-21 12:02:47 +0100

    parsing element data and creating project list

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,19 @@
 class Tooltip {}
 
-class ProjectList {}
+class ProjectList {
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    console.log(prjItems);
+  }
+}
 
 class ProjectItem {}
 
-class App {}
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
