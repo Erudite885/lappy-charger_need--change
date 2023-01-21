5aa24e7b6762e505f3ea1bd3edfa1ad236573e7c
Erudite885 <erudite885@gmail.com>
2023-01-21 12:23:42 +0100

    switch button logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,41 @@
 class Tooltip {}
 
-class ProjectList {
-  constructor(type) {
-    const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectSwitchBtn();
+    this.connectMoreInfoBtn();
+  }
+
+  connectMoreInfoBtn() {}
+
+  connectSwitchBtn() {
+    const prjItemElement = document.getElementById(this.id);
+    const switchBtn = prjItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
   }
 }
 
-class ProjectItem {}
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    // console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
 
 class App {
   static init() {
